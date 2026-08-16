0fd5287cc81b239b42b3a1a9edd07060177a6080
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-08-16 16:05:28 +0500

    Add Bearer Token header for mobile CORS bypass

--- src/api/axios.js
+++ src/api/axios.js
@@ -9,6 +9,10 @@ const API = axios.create({
 API.interceptors.request.use(
   (config) => {
     config.withCredentials = true;
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
     return config;
   },
   (error) => Promise.reject(error)
